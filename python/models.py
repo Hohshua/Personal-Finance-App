46d3b4752e27be291419e60590b1a88635bd04e5
Hohshua <hohshua69@gmail.com>
2023-11-21 16:50:46 -0600

    Populated models.py

--- python/models.py
+++ python/models.py
@@ -7,10 +7,51 @@ db = SQLAlchemy()
 
 class UserInfo(db.Model):
     __tablename__ = 'user_info'
-    access_token = db.Column(db.String(100), primary_key=True)
+    id = db.Column(db.String(100), primary_key=True)
     item_id = db.Column(db.String(100))
     account = db.relationship('Account', backref='user_info')
 
+
+# http://localhost:3000/api/accounts
 class Account(db.Model):
     __tablename__ = 'account'
-    
+    id = db.Column(db.String(100), primary_key=True)
+    name = db.Column(db.String(50))
+    currency = db.Column(db.String(5))
+    balance = db.Column(db.Integer)
+    acc_type = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey('user_info.id'))
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'currency': self.currency,
+            'balance': self.balance,
+            'acc_type': self.acc_type,
+            'user_id': self.user_id
+        }
+
+
+# http://localhost:3000/api/transactions
+class Transaction(db.Model):
+    __tablename__ = 'transaction'
+    id = db.Column(db.String(100), primary_key=True)
+    name = db.Column(db.String(50))
+    category = db.Column(db.String(50))
+    amount = db.Column(db.Integer)
+    logo_url = db.Column(db.String(100))
+    website = db.Column(db.String(100))
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'category': self.category,
+            'amount': self.amount,
+            'logo_url': self.logo_url,
+            'website': self.website,
+            'account_id': self.account_id
+        }
+
